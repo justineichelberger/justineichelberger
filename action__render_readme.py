bbcddc69846467c0a1cce98ced02f7a80d6f3cef
justineichelberger <justin.g.eichelberger@gmail.com>
2022-04-22 17:36:18 -0600

    trying to fix new timestamp for most recent update detail

--- action__render_readme.py
+++ action__render_readme.py
@@ -10,16 +10,9 @@ markdown_text = open("./README.template.md", "r").read()
 markdown_text_for_template += f"<sub>On this day in {response.json()[0][8]}, an anonymous witness in {response.json()[0][1]} ({response.json()[0][2]}) reported seeing craft that fit the description of some '{response.json()[0][3]}' over a period of approximately {response.json()[0][11]} - {response.json()[0][11]} minutes.</sub><blockquote><sub>{response.json()[0][-1]}</sub></blockquote><sub><sub><i>source: [{response.json()[0][-2]}]({response.json()[0][-2]})</i></sub></sub></sub><br>" 
 print(markdown_text_for_template)
 
-new_markdown_text = markdown_text.replace("{random_sighting}", markdown_text_for_template)
+timestamp_for_last_update = f'Last Updated: {datetime.datetime.now()}'
+
+new_markdown_text = markdown_text.replace("{random_sighting}", markdown_text_for_template).replace("{timestamp_for_last_update}", markdown_text_for_template)
 
 new_readme = open("README.md", "w")
 new_readme.write(new_markdown_text)
-
-timestamp_for_last_update = f'Last Updated: {datetime.datetime.now()}'
-
-timestamp_for_last_update_markdown_text = markdown_text.replace("{timestamp_for_last_update}", markdown_text_for_template)
-
-new_readme = open("README.md", "w")
-new_readme.write(timestamp_for_last_update_markdown_text)
-
-print(datetime.datetime.now())
